817a96d8f83e24e8f9c1c73e688e38b66af135e7
Queralt Sosa Mompel <qsosa@caisistemas.es>
2025-04-09 11:27:34 +0200

    Updating footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 function Footer() {
   return (
     <footer>
-      <h3><span className="highlight-1">Queralt Sosa Mompel</span></h3>
-      <p>Personal website</p>
+      <h3><span className="highlight-1">queraltsm</span></h3>
+      <p>Personal website of <strong>Queralt Sosa Mompel</strong>.</p>
     </footer>
   );
 }
