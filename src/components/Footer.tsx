d7846cf02bad0597c5826210318c2be05fed3c4e
Queralt Sosa Mompel <qsosa@caisistemas.es>
2025-04-09 12:13:51 +0200

    Social links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,22 @@
 function Footer() {
   return (
+    <>
+    <div className="social-bar">
+    <a href="https://github.com/queraltsm" target="_blank">
+        <img src="https://cdn-icons-png.flaticon.com/512/733/733553.png" alt="GitHub" className="icon" />
+    </a>
+    <a href="https://www.linkedin.com/in/queraltsm/" target="_blank">
+        <img src="https://cdn-icons-png.flaticon.com/512/733/733561.png" alt="LinkedIn" className="icon" />
+    </a>
+    <a href="mailto:queraltsm@outlook.com">
+        <img src="https://cdn-icons-png.flaticon.com/512/732/732200.png" alt="Email" className="icon" />
+    </a>
+    </div>
     <footer>
       <h3><span className="footer-title">queraltsm</span></h3>
       <p className="footer-text">Personal website of <strong>Queralt Sosa Mompel</strong>.</p>
     </footer>
+    </>
   );
 }
 
