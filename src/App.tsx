8e3971ad9fc9d9d64649abe0f4f80e82f60e1e5a
Queralt Sosa Mompel <qsosa@caisistemas.es>
2025-04-13 15:01:26 +0200

    Contact page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Usar Routes en lugar de Switch
 import Header from './components/Header';
 import Home from './components/Home';
-import Skills from './components/Skills';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <div className="container">
-        <Home />
-        <div className="skills">
-          <Skills />
-        </div>
-      </div>
-      <br /><br /><br /><br /><br /><br /><br /><br />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       <Footer />
-    </>
+    </Router>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,49 @@
+import { Link, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
 function Header() {
+    const location = useLocation();
+    const [activeLink, setActiveLink] = useState('home');
+
+    useEffect(() => {
+        if (location.pathname === '/') {
+            setActiveLink('home');
+        } else if (location.pathname === '/contact') {
+            setActiveLink('contact');
+        }
+    }, [location]);
+
     return (
-        <header className="header">
-            <div className="header-content">
-                <h1 className="name">Queralt Sosa Mompel</h1>
-                <p className="role-line">
-                    <span className="highlight-1">Software Engineer</span>
-                </p>
-            </div>
-        </header>
+        <>
+            <header className="header">
+                <div className="header-content">
+                    <h1 className="name">Queralt Sosa Mompel</h1>
+                    <p className="role-line">
+                        <span className="highlight-1">Software Engineer</span>
+                    </p>
+                </div>
+            </header>
+            <nav className="nav-menu">
+                <ul>
+                    <li>
+                        <Link 
+                            to="/"
+                            className={activeLink === 'home' ? 'active' : ''}
+                        >
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link 
+                            to="/contact"
+                            className={activeLink === 'contact' ? 'active' : ''}
+                        >
+                            Contact
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </>
     );
 }
 
